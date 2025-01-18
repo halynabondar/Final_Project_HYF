4f55421b7a661d5746bd12fc86ec908750bb3f04
markitosha <nataly_106@mail.ru>
2025-01-18 22:20:41 +0100

    Added table creation

--- app/app/api/data/route.js
+++ app/app/api/data/route.js
@@ -3,8 +3,14 @@ import knex from '@/app/api/knex';
 
 export async function GET() {
   try {
-    await knex('data').insert({ data: 'test' });
-    const data = await knex('data').select('*');
+    await knex.schema.createTableIfNotExists('test', function (table) {
+      table.increments();
+      table.string('name');
+      table.timestamps();
+    });
+
+    await knex('test').insert({ name: 'test' });
+    const data = await knex('test').select('*');
 
     return NextResponse.json(data);
   } catch (err) {
