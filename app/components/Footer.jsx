18e35bf168bf14bbd761837f27cb49d1bee84325
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-21 09:36:05 +0100

    Fix footer styles

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,18 +1,14 @@
-import React from "react";
-
 const Footer = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex-grow">
+    <footer className="w-full bg-blue-100 py-7">
+      <div className="container mx-auto flex items-center justify-center gap-10">
+        <p className="text-sm">
+          &copy; {new Date().getUTCFullYear()} CodeCrafters Inc.
+        </p>
+        <p className="text-sm">Privatlivspolitik</p>
+        <p className="text-sm">Copyright</p>
       </div>
-      <footer className="absolute bottom-0 w-full h-10 bg-gray-100 text-black py-4 border-t">
-        <div className="container mx-auto flex justify-between items-center">
-          <p className="text-sm">
-          &copy; {new Date().getUTCFullYear()} CodeCrafters Inc. | Privatlivspolitik | Copyright
-          </p>
-        </div>
-      </footer>
-    </div>
+    </footer>
   );
 };
 
