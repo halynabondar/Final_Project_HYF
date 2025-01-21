e5b8a9fd8e62849d1be9a6112ed95489a7be3e40
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-21 14:32:58 +0100

    Fix footer styles

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,11 +2,11 @@ const Footer = () => {
   return (
     <footer className="w-full bg-blue-100 py-7">
       <div className="container mx-auto flex items-center justify-center gap-10">
-        <p className="text-sm">
+        <p className="text-sm font-semibold">
           &copy; {new Date().getUTCFullYear()} CodeCrafters Inc.
         </p>
-        <p className="text-sm">Privatlivspolitik</p>
-        <p className="text-sm">Copyright</p>
+        <p className="text-sm font-semibold">Privatlivspolitik</p>
+        <p className="text-sm font-semibold">Copyright</p>
       </div>
     </footer>
   );
