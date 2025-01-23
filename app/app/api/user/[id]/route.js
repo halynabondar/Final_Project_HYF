2957dc9c78254e267669b3dbdc96d1afcdceed59
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-23 21:41:48 +0100

    Add PUT and POST endpoints

--- app/app/api/user/[id]/route.js
+++ app/app/api/user/[id]/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import knex from '@/app/api/knex';
 
+// GET endpoint: Fetch user by ID
 export async function GET(req, { params }) {
   const { id } = params; // Extract `id` from the URL
 
@@ -17,6 +18,48 @@ export async function GET(req, { params }) {
     console.error('Error fetching user:', error.message);
     return NextResponse.json(
       { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
+
+// PUT endpoint: Update user data
+export async function PUT(req, { params }) {
+  const { id } = params;
+  const body = await req.body;
+
+  try {
+    const updatesRows = await knex('users').where({ id }).update(body);
+
+    if (!updatesRows) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+    const updateUser = await knex('users').where({ id }).first();
+    return NextResponse.json(updateUser);
+  } catch (error) {
+    console.error('Error updating user:', error.message);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
+
+// POST endpoint: Update image
+export async function POST(req, { params }) {
+  const { id } = params;
+
+  try {
+    const formData = await req.formData();
+    const file = formData.get('image');
+
+    console.log(`Received file for user ID: ${id}`, file);
+
+    return NextResponse.json({ message: 'File uploaded successfully' });
+  } catch (error) {
+    console.error('Error uploading file:', error.message);
+    return NextResponse.json(
+      { error: 'Error uploading file' },
       { status: 500 },
     );
   }
