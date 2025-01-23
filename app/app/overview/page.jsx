894fc9846a06e0c2f251bbe60460f63e63551a12
SpringrollBrothers <luliu.vln@gmail.com>
2025-01-23 11:17:02 +0100

    fix typo with linter

--- app/app/overview/page.jsx
+++ app/app/overview/page.jsx
@@ -1,13 +1,13 @@
-'use client'
-import React, { useState } from "react";
-import clsx from "clsx";
+'use client';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 const OverviewPage = () => {
   const [questions, setQuestions] = useState(
     Array.from({ length: 40 }, (_, i) => ({
       id: i + 1,
-      answered: i ===0,//first question as answered
-    }))
+      answered: i === 0, //first question as answered
+    })),
   );
 
   const handleQuestionClick = (id) => {
@@ -15,23 +15,23 @@ const OverviewPage = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen p-10 ">
+    <div className="flex min-h-screen items-center justify-center p-10 ">
       <div className="flex max-w-7xl space-x-6">
         {/* Left Section: Text */}
         <div className="w-1/2">
-          <h1 className="text-2xl font-bold mb-4">Din spørgsmåloversigt</h1>
+          <h1 className="mb-4 text-2xl font-bold">Din spørgsmåloversigt</h1>
           <p>
             Spørgsmåloversigten giver dig et overblik over dine besvarede og
             ubesvarede spørgsmål. Du skal trykke på et nummer for at gå til
             spørgsmålet.
           </p>
           <div>
-            <div className="flex items-center mb-2">
-              <div className="w-6 h-6 border border-gray-500 mr-2"></div>
+            <div className="mb-2 flex items-center">
+              <div className="mr-2 size-6 border border-gray-500"></div>
               <span>Ikke besvaret</span>
             </div>
             <div className="flex items-center">
-              <div className="w-6 h-6 bg-gray-400 mr-2"></div>
+              <div className="mr-2 size-6 bg-gray-400"></div>
               <span>Besvaret</span>
             </div>
           </div>
@@ -42,20 +42,20 @@ const OverviewPage = () => {
           <div
             className="grid gap-1"
             style={{
-              gridTemplateColumns: "repeat(10, 3rem)", 
-              gridTemplateRows: "repeat(4, 3rem)",  
+              gridTemplateColumns: 'repeat(10, 3rem)',
+              gridTemplateRows: 'repeat(4, 3rem)',
             }}
           >
             {questions.map((question) => (
-              <button 
+              <button
                 key={question.id}
                 onClick={() => handleQuestionClick(question.id)}
                 className={clsx(
-                  'w-full h-full border border-gray-500 rounded text-center', 
+                  'size-full rounded border border-gray-500 text-center',
                   {
                     'bg-gray-400 border-gray-400': question.answered, // answered
-                    'border-gray-500': !question.answered //not answered
-                  }
+                    'border-gray-500': !question.answered, //not answered
+                  },
                 )}
               >
                 {question.id}
