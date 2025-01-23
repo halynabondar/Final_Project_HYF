b253d3671d9174998a94f0858de8f59b5d8d5421
SpringrollBrothers <luliu.vln@gmail.com>
2025-01-23 16:19:42 +0100

    fix double quotes

--- app/app/overview/page.jsx
+++ app/app/overview/page.jsx
@@ -15,32 +15,32 @@ const OverviewPage = () => {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center p-10 ">
-      <div className="flex max-w-7xl space-x-6">
+    <div className='flex min-h-screen items-center justify-center p-10 '>
+      <div className='flex max-w-7xl space-x-6'>
         {/* Left Section: Text */}
-        <div className="w-1/2">
-          <h1 className="mb-4 text-2xl font-bold">Din spørgsmåloversigt</h1>
+        <div className='w-1/2'>
+          <h1 className='mb-4 text-2xl font-bold'>Din spørgsmåloversigt</h1>
           <p>
             Spørgsmåloversigten giver dig et overblik over dine besvarede og
             ubesvarede spørgsmål. Du skal trykke på et nummer for at gå til
             spørgsmålet.
           </p>
           <div>
-            <div className="mb-2 flex items-center">
-              <div className="mr-2 size-6 border border-gray-500"></div>
+            <div className='mb-2 flex items-center'>
+              <div className='mr-2 size-6 border border-gray-500'></div>
               <span>Ikke besvaret</span>
             </div>
-            <div className="flex items-center">
-              <div className="mr-2 size-6 bg-gray-400"></div>
+            <div className='flex items-center'>
+              <div className='mr-2 size-6 bg-gray-400'></div>
               <span>Besvaret</span>
             </div>
           </div>
         </div>
 
         {/* Right Section: Grid */}
-        <div className="w-1/2">
+        <div className='w-1/2'>
           <div
-            className="grid gap-1"
+            className='grid gap-1'
             style={{
               gridTemplateColumns: 'repeat(10, 3rem)',
               gridTemplateRows: 'repeat(4, 3rem)',
