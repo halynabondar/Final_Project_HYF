cd0fb50ae748b87aec2cfcade08ed017fb4766d5
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-24 14:18:17 +0100

    Fix autoComplete

--- app/app/signup/page.jsx
+++ app/app/signup/page.jsx
@@ -70,7 +70,7 @@ export default function SignUpPage() {
                 type="town"
                 name="town"
                 id="town"
-                autoComplete="city"
+                autoComplete="address-level-12"
                 required
                 placeholder="Indtast by"
               />
