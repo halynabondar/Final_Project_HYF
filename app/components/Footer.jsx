7206d05ef9f6620c7e8516a21b4575d02e59fb4d
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-24 20:31:06 +0100

    Change text color

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full bg-blue-100 py-7">
+    <footer className="w-full bg-blue-100 py-7 text-gray-700">
       <div className="container mx-auto flex items-center justify-center gap-10">
         <p className="text-sm font-semibold">
           &copy; {new Date().getUTCFullYear()} CodeCrafters Inc.
