173105d4abd30641b9d33796ab22a56dec79e376
SpringrollBrothers <luliu.vln@gmail.com>
2025-01-26 12:29:36 +0100

    fixed lint error

--- app/app/result/page.jsx
+++ app/app/result/page.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import clsx from 'clsx'
-import { style } from '@/app/style';
+import clsx from 'clsx';
+
 import {
   TableContainer,
   Table,
@@ -10,14 +9,13 @@ import {
   TableCell,
 } from '@mui/material';
 
-
 export default function ResultsPage() {
   return (
     <div className="mx-auto max-w-4xl rounded-2xl bg-white p-10 shadow-xl">
-      <h1 className='text-center'>Dine Resultater</h1>
+      <h1 className="text-center">Dine Resultater</h1>
 
       {/* User Results Table */}
-      <div className="bg-white shadow-md rounded-lg p-6 mb-10">
+      <div className="mb-10 rounded-lg bg-white p-6 shadow-md">
         <TableContainer>
           <Table className="w-full border-collapse border border-gray-300 text-center ">
             {/* Table Header */}
@@ -42,51 +40,40 @@ export default function ResultsPage() {
 
             <TableBody>
               <TableRow>
-                <TableCell className="border border-gray-300 px-4 py-2">12.3.2024</TableCell>
-                <TableCell className="border border-gray-300 px-4 py-2 flex items-center justify-center gap-2">
+                <TableCell className="border border-gray-300 px-4 py-2">
+                  12.3.2024
+                </TableCell>
+                <TableCell className="flex items-center justify-center gap-2 border border-gray-300 px-4 py-2">
                   <span>Full Name</span>
                 </TableCell>
-                <TableCell className="border border-gray-300 px-4 py-2">36</TableCell>
-                <TableCell className="border border-gray-300 px-4 py-2">4</TableCell>
+                <TableCell className="border border-gray-300 px-4 py-2">
+                  36
+                </TableCell>
+                <TableCell className="border border-gray-300 px-4 py-2">
+                  4
+                </TableCell>
               </TableRow>
             </TableBody>
-          </ Table>
+          </Table>
         </TableContainer>
       </div>
 
       {/* Incorrect Answers Section */}
-      <div className="bg-white shadow-md rounded-lg p-6">
-        <h2 className='text-center text-black text-bold'>Forkerte svar</h2>
+      <div className="rounded-lg bg-white p-6 shadow-md">
+        <h2 className=" text-center text-black">Forkerte svar</h2>
         <div className="grid grid-cols-2 gap-3">
           {/* Example of One Answer Block */}
           {Array.from({ length: 6 }).map((_, index) => (
-            <div
-              key={index}
-              className={clsx(
-                'p-4 rounded-lg bg-gray-100'
-              )}
-            >
+            <div key={index} className={clsx('rounded-lg bg-gray-100 p-4')}>
               {/* question  */}
-              <h3
-                className={clsx(
-                  'text-lg font-semibold mb-2',
-                  'text-black'
-                )}
-              >
+              <h3 className={clsx('mb-2 text-lg font-semibold', 'text-black')}>
                 {index + 1}. Question
               </h3>
               <p className="text-red-500">
-                <span className="font-semibold">Dit svar: </span>
-                A : Spain
+                <span className="font-semibold">Dit svar: </span>A : Spain
               </p>
-              <p
-                className={clsx(
-                  'text-green-500',
-                  'font-semibold'
-                )}
-              >
-                <span>Korrekt svar: </span>
-                C : Germany
+              <p className={clsx('text-green-500', 'font-semibold')}>
+                <span>Korrekt svar: </span>C : Germany
               </p>
             </div>
           ))}
