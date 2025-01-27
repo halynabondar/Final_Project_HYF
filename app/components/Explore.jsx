46dd0cf8a429a940c09a0eb578c3150ab7df9244
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-27 16:33:38 +0100

    Fix icon size

--- app/components/Explore.jsx
+++ app/components/Explore.jsx
@@ -4,7 +4,7 @@ export default function Explore({ icon, title }) {
   return (
     <>
       <div className="flex min-h-[150px] min-w-[150px] cursor-pointer flex-col items-center justify-center gap-3 rounded-2xl bg-blue-100 p-3 text-center duration-500 hover:bg-blue-200">
-        <div className="flex size-[50px] items-center justify-center rounded-full bg-blue-500 p-3 text-blue-50">
+        <div className="flex size-12 items-center justify-center rounded-full bg-blue-500 p-3 text-blue-50">
           {icon}
         </div>
         <h3 className="text-lg font-semibold">{title}</h3>
