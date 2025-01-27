d7afc53fc2c94924508b29eeb0f38e0818bfa4cc
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-27 16:17:55 +0100

    - Use {icon} instead of FontAwesomeIcon

--- app/components/Explore.jsx
+++ app/components/Explore.jsx
@@ -1,12 +1,11 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 
 export default function Explore({ icon, title }) {
   return (
     <>
       <div className="flex min-h-[150px] min-w-[150px] cursor-pointer flex-col items-center justify-center gap-3 rounded-2xl bg-blue-100 p-3 text-center duration-500 hover:bg-blue-200">
-        <div className="flex size-[60px] items-center justify-center rounded-full bg-blue-500 p-3">
-          <FontAwesomeIcon className="w-10 text-blue-100" icon={icon} />
+        <div className="flex size-[50px] items-center justify-center rounded-full bg-blue-500 p-3 text-blue-50">
+          {icon}
         </div>
         <h3 className="text-lg font-semibold">{title}</h3>
       </div>
