3fa98dbb450167401e17fa57dd6dd8fd391d2e0b
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-27 16:34:16 +0100

    Fix responsiveness

--- app/app/start/page.jsx
+++ app/app/start/page.jsx
@@ -33,8 +33,8 @@ function WelcomePage() {
   return (
     <section>
       <div className="flex flex-col items-center justify-center gap-10 py-2 sm:py-4 md:flex-row">
-        <div className="flex max-w-[600px] flex-col items-center justify-center md:max-w-[400px] lg:max-w-[600px]">
-          <h1 className="xs:text-[48px] xs:leading-[77px] w-full font-poppins text-[40px] font-semibold leading-[67px]">
+        <div className="flex max-w-[600px] flex-col items-center justify-center md:max-w-screen-xs lg:max-w-[600px]">
+          <h1 className="w-full font-poppins text-[40px] font-semibold leading-[67px] xs:text-center xs:text-[48px] xs:leading-[77px] ss:text-center md:text-left">
             Velkommen <br /> til DKTestPrep
           </h1>
           <p className="font-poppins text-[18px] font-normal leading-[30px]">
@@ -59,7 +59,7 @@ function WelcomePage() {
           />
         </div>
       </div>
-      <div className="ss:flex-wrap xs:flex-wrap ss:gap-10 xs:gap-8 my-4 flex flex-row justify-center sm:my-10 sm:flex-wrap sm:gap-10 md:gap-20">
+      <div className="my-4 flex flex-row justify-center xs:flex-wrap xs:gap-3 ss:gap-10 sm:my-10 sm:flex-wrap sm:gap-10 md:gap-20">
         {exploreFeatures.map((feature) => (
           <Explore key={feature.id} icon={feature.icon} title={feature.title} />
         ))}
