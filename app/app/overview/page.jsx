1c7b874ed22c6b0e5270160fc738c7eb88da6582
SpringrollBrothers <luliu.vln@gmail.com>
2025-01-29 11:12:12 +0100

    fixed page responsiveness, added margin on text<span>

--- app/app/overview/page.jsx
+++ app/app/overview/page.jsx
@@ -16,23 +16,23 @@ const OverviewPage = () => {
 
   return (
     <div className="flex min-h-screen items-center justify-center p-10 ">
-      <div className="flex max-w-7xl space-x-6">
+      <div className="flex max-w-7xl flex-col space-y-6 md:flex-row md:space-x-6 md:space-y-0">
         {/* Left Section: Text */}
-        <div className="w-1/2">
-          <h1 className="mb-4 text-2xl font-bold">Din spørgsmåloversigt</h1>
-          <p>
+        <div className="w-full md:w-1/2">
+          <h1 className="mb-4 text-3xl font-bold">Din spørgsmåloversigt</h1>
+          <p className="text-lg">
             Spørgsmåloversigten giver dig et overblik over dine besvarede og
             ubesvarede spørgsmål. Du skal trykke på et nummer for at gå til
             spørgsmålet.
           </p>
           <div>
             <div className="mb-2 flex items-center">
-              <div className="mr-2 size-6 border border-gray-500"></div>
-              <span>Ikke besvaret</span>
+              <div className="mr-2 mt-7 size-6 rounded border border-gray-500 p-2"></div>
+              <span className="mt-7 ">Ikke besvaret</span>
             </div>
             <div className="flex items-center">
-              <div className="mr-2 size-6 bg-gray-400"></div>
-              <span>Besvaret</span>
+              <div className="mr-2 mt-1 size-6 rounded bg-gray-400 p-2"></div>
+              <span className="mt-1 ">Besvaret</span>
             </div>
           </div>
         </div>
@@ -40,10 +40,9 @@ const OverviewPage = () => {
         {/* Right Section: Grid */}
         <div className="w-1/2">
           <div
-            className="grid gap-1"
+            className="grid gap-4 "
             style={{
-              gridTemplateColumns: 'repeat(10, 3rem)',
-              gridTemplateRows: 'repeat(4, 3rem)',
+              gridTemplateColumns: 'repeat(auto-fit, minmax(2.5rem, 1fr))',
             }}
           >
             {questions.map((question) => (
@@ -51,7 +50,7 @@ const OverviewPage = () => {
                 key={question.id}
                 onClick={() => handleQuestionClick(question.id)}
                 className={clsx(
-                  'size-full rounded border border-gray-500 text-center',
+                  'aspect-square rounded border border-gray-500 text-center',
                   {
                     'bg-gray-400 border-gray-400': question.answered, // answered
                     'border-gray-500': !question.answered, //not answered
