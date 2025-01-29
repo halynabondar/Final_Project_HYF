95e360acc4453db7e49f4a4efbaaa627d5f1c9ad
SpringrollBrothers <luliu.vln@gmail.com>
2025-01-29 16:33:25 +0100

    small improvement on gaps and padding

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="w-full bg-blue-100 py-7 text-gray-700">
-      <div className="container mx-auto flex flex-col items-center justify-center gap-4 text-center sm:flex-row sm:gap-10 sm:text-left">
+    <footer className="w-full bg-blue-100 py-4 text-gray-700">
+      <div className="container mx-auto flex flex-col items-center justify-center gap-1 text-center sm:flex-row sm:gap-10 sm:text-left">
         <p className="text-sm font-semibold">
           &copy; {new Date().getUTCFullYear()} CodeCrafters Inc.
         </p>
