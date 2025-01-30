41aa9bbd56620bc89a39f3f67f8ea7db3d6af2dc
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-30 10:09:26 +0100

    Fetch user's data and add AUTH_SECRET

--- app/auth.js
+++ app/auth.js
@@ -1,20 +1,43 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import knex from '@/app/api/knex';
+import bcrypt from 'bcrypt';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
       credentials: {
-        email: {},
-        password: {},
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
       },
-      authorize: async (credentials) => {
-        console.log('Credentials', credentials);
+      async authorize(credentials) {
+        console.log('Credentials:', credentials);
 
-        return { id: 1, name: 'John Doe', email: 'test@test.com' };
+        try {
+          const { email, password } = credentials;
+
+          const user = await knex('users')
+            .select('email', 'password')
+            .where({ email })
+            .first();
+
+          if (!user) {
+            throw new Error('Invalid email or password');
+          }
+
+          // Compare hashed password using bcrypt
+          const isValidPassword = await bcrypt.compare(password, user.password);
+          if (!isValidPassword) {
+            throw new Error('Invalid email or password');
+          }
+
+          return { id: user.id, name: user.name, email: user.email };
+        } catch (error) {
+          console.error('Error fetching user:', error.message);
+          return null;
+        }
       },
     }),
   ],
+  secret: process.env.AUTH_SECRET,
 });
