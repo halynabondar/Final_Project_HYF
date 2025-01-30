29c7128356d1cd8b9866257caf94f22f649ee9fc
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-30 17:27:34 +0100

    Add connection to the database

--- app/utils/db.js
+++ app/utils/db.js
@@ -1,12 +1,25 @@
-import knex from '@/app/api/knex';
+import knex from 'knex';
+
+export const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false },
+  },
+  pool: { min: 2, max: 10 },
+});
 
 export const getUserFromDb = async (email) => {
   try {
     // Query the database for a user with the given email
-    const user = await knex('users')
-      .select('email', 'password')
+    const user = await db('users')
+      .select('email', 'password_hash', 'salt')
       .where({ email })
       .first();
+
     return user;
   } catch (error) {
     console.error('Error fetching user:', error.message);
