8c14d27d129068f5119793b18f5d50cfe3c000ac
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-30 17:30:40 +0100

    Update salt and hash

--- app/utils/password.js
+++ app/utils/password.js
@@ -2,9 +2,11 @@ import bcrypt from 'bcryptjs';
 
 export const saltAndHashPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
-  return bcrypt.hash(password, salt);
+  const hash = await bcrypt.hash(password + salt, 10);
+  return { salt, hash };
 };
 
-export const verifyPassword = async (password, hashedPassword) => {
-  return bcrypt.compare(password, hashedPassword);
+export const verifyPassword = async (password, salt, hashedPassword) => {
+  const hash = await bcrypt.hash(password + salt, 10);
+  return hash === hashedPassword;
 };
