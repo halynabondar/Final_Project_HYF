966a2681243304350471840ad58fa203a2b48fc7
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-31 17:52:15 +0100

    Delete user endpoint added

--- app/app/api/private/user/[id]/route.js
+++ app/app/api/private/user/[id]/route.js
@@ -43,3 +43,34 @@ export async function PUT(req, { params }) {
     );
   }
 }
+
+export async function DELETE(req, { params }) {
+  const { id } = await params;
+
+  try {
+    // Fetch the user with the given id
+    const user = await knex('users').where({ email: id }).first();
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    // delete related content
+    // auth records
+    await knex('accounts').where({ userId: user.id }).del();
+    await knex('sessions').where({ userId: user.id }).del(); // should also destroy session
+    // test related data
+    await knex('result_history').where({ user_id: user.id }).del();
+    await knex('test_sessions').where({ user_id: user.id }).del();
+    // delete user itself
+    await knex('users').where({ id: user.id }).del();
+
+    return NextResponse.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Error fetching user:', error.message);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
