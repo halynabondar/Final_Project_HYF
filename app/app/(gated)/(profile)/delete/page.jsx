accf1ae5d1fda92c935119677ec581c533b0a540
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-31 14:31:02 +0100

    Replace button's styles and fix autoComplete

--- app/app/(gated)/(profile)/delete/page.jsx
+++ app/app/(gated)/(profile)/delete/page.jsx
@@ -30,31 +30,29 @@ export default function DeleteAccount() {
 
   return (
     <div className="mt-5 flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-5 rounded-xl bg-blue-50 p-5">
-        <div className="flex size-14 items-center justify-center rounded-full bg-gray-200">
+      <div className="flex flex-col items-center justify-center gap-5 rounded-xl bg-blue-50 p-5 shadow-xl">
+        <div className="flex size-14 items-center justify-center rounded-full bg-blue-100">
           <DeleteForeverIcon className="text-3xl text-red-500" />
         </div>
-        <h1 className="my-2 text-3xl font-bold text-blue-700">Slet konto</h1>
+        <h1 className="my-1 text-3xl font-bold">Slet konto</h1>
         <p className="text-center text-lg text-red-700">
           <b>ADVARSEL</b>
           <br /> dette er permanent og kan ikke fortrydes!
         </p>
-        <p className="text-lg">
-          Alle resultathistorikker vil blive slettet med det samme.
-        </p>
         <div className="">
           <label htmlFor="delete" className="font-bold">
-            Bekræft brugernavn
+            Bekræft dit email
           </label>
           <input
+            autoComplete="email"
             id="delete"
             name="delete"
-            className="mt-2 w-full rounded-xl px-3 py-2"
-            placeholder="Іndtast dit fornavn og efternavn"
+            className="mt-2 w-full rounded-xl border-2 px-3 py-2"
+            placeholder="Іndtast dit email"
           ></input>
         </div>
         <div className="mt-2 flex gap-5">
-          <Button value="Slet konto" onClick={handleDelete} />
+          <Button value="Slet konto" variant="delete" onClick={handleDelete} />
         </div>
       </div>
     </div>
