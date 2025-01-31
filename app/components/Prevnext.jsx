1aa0bad80561ea0dde932d4f4f27f0e7694fe93f
Dennis Agyekum <kinggbadi@gmail.com>
2025-01-31 09:57:38 +0100

    some syling added to improve responsiveness

--- app/components/Prevnext.jsx
+++ app/components/Prevnext.jsx
@@ -3,21 +3,25 @@ import PropTypes from 'prop-types';
 
 function PreviousNext({ showPrevious, showNext, onPrevious, onNext }) {
   return (
-    <div className="mx-auto mt-12 flex max-w-[738px] justify-between">
-      {showPrevious && (
+    <div className="mx-auto mt-6 flex w-full max-w-[738px] items-center justify-between">
+      {showPrevious ? (
         <Button
-          styles="bg-blue-500 hover:bg-blue-700"
-          value="Previous"
+          styles="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700"
+          value="Tilbage"
           onClick={onPrevious}
         />
+      ) : (
+        <div className="w-24" />
       )}
-      <div className="flex-1"></div>
-      {showNext && (
+
+      {showNext ? (
         <Button
-          styles="pl-11 pr-11 bg-blue-500 hover:bg-blue-700"
-          value="Next"
+          styles=" pl-10 pr-100 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700"
+          value="Næste"
           onClick={onNext}
         />
+      ) : (
+        <div className="w-24" />
       )}
     </div>
   );
