c6cc83193ed0271ce3db549cebc98719c91f76d4
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-31 15:01:34 +0100

    Get results only for specified user

--- app/app/api/results/route.js
+++ app/app/api/results/route.js
@@ -1,9 +1,21 @@
 import { NextResponse } from 'next/server';
 import knex from '@/app/api/knex';
 
-export async function GET() {
+export async function GET(req) {
   try {
-    const result = await knex('result_history').select('*');
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('userId');
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: 'User ID is required' },
+        { status: 400 },
+      );
+    }
+
+    const result = await knex('result_history')
+      .where({ user_id: userId })
+      .select('*');
 
     return NextResponse.json(result);
   } catch (error) {
