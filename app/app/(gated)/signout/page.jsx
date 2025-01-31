a39bc60ead96e9bf68a5b784809dd429ae98aeef
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-31 19:07:25 +0100

    Fix Sign out form

--- app/app/(gated)/signout/page.jsx
+++ app/app/(gated)/signout/page.jsx
@@ -1,18 +1,18 @@
-import { signOut } from '@/auth';
+import { signOut } from 'next-auth/react';
 import Button from '@/components/Button';
 
 export default function SignOutPage() {
+  const handleSignOut = (e) => {
+    e.preventDefault();
+    signOut();
+  };
+
   return (
     <section className="flex flex-col items-center justify-center gap-8 pt-10">
       <h5 className="w-full text-center font-poppins text-[28px] font-semibold leading-[40px] xs:text-[34px] xs:leading-[50px]">
         Er du sikker på, at du vil logge ud?
       </h5>
-      <form
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
-      >
+      <form onSubmit={handleSignOut}>
         <Button type="submit" aria-label="Log out" value="Log ud" />
       </form>
     </section>
