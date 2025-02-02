0548499b12abff8ba706af5cad96e7e5e23033cf
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-02-02 18:11:24 +0100

    Fix login

--- app/utils/password.js
+++ app/utils/password.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 
-export const saltAndHashPassword = async (password, salt) => {
-  return await bcrypt.hash(password + salt, 10);
+export const saltAndHashPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
 };
 
 export const verifyPassword = async (password, hashedPassword) => {
