c6e5d8817d0f89cf35b4f14becc612735436392c
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-02-02 18:16:32 +0100

    Fix login

--- app/app/api/auth/signup/route.js
+++ app/app/api/auth/signup/route.js
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import { saltAndHashPassword } from '@/utils/password';
 import { db } from '@/utils/db';
-import { auth } from '@/auth';
 
 export async function POST(req) {
   try {
