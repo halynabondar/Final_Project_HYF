c726651ce4fbe1a803457e64b37cc28504b84bfb
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-02-02 20:59:12 +0100

    Layout fixes

--- app/components/Explore.jsx
+++ app/components/Explore.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export default function Explore({ icon, title }) {
   return (
     <>
-      <div className="flex min-h-[150px] min-w-[150px] cursor-pointer flex-col items-center justify-center gap-3 rounded-2xl bg-blue-100 p-3 text-center duration-500 hover:bg-blue-200">
+      <div className="flex min-h-[150px] min-w-[150px] flex-col items-center justify-center gap-3 rounded-2xl bg-blue-100 p-3 text-center duration-500 hover:bg-blue-200">
         <div className="flex size-12 items-center justify-center rounded-full bg-blue-500 p-3 text-blue-50">
           {icon}
         </div>
